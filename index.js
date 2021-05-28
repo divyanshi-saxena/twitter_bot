251530d2b939f1753a8c940a7ade1a72c07ded3b
divyanshi-saxena <divyanshi.saxena0204@gmail.com>
2021-05-28 10:59:20 +0530

    updated indexjs

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ require('./config/passport')(passport)
 
 // MIDDLEWARES
 app.use(express.static('public'))
-// app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
 // VIEW ENGINE
 app.set('view engine', 'ejs')
@@ -30,7 +30,7 @@ app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: false,  
-  cookie: { secure: true }, 
+  // cookie: { secure: true }, 
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI
   })
